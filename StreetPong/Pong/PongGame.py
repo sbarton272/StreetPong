50686ae96fd104cac6838851956196f32580e077
Spencer Barton <sbarton272@gmail.com>
2015-01-11 23:33:03 -0500

    Working on pong model

--- StreetPong/Pong/PongGame.py
+++ StreetPong/Pong/PongGame.py
@@ -6,6 +6,25 @@ Leanna Pancoast, Spencer Barton, Justin Frye
 Pong Game
 """
 
+#=====================================================
+# Imports
+#=====================================================
+
+import random as rnd
+import math
+
+#=====================================================
+# Constants
+#=====================================================
+
+# TODO figure out how else to get this const
+STARTING_VELOCITY_MAGNITUDE = 3
+STARTING_VELOCITY_ANGLE = 30 # degrees
+
+#=====================================================
+# PongGame
+#=====================================================
+
 class PongGame(object):
     """
     PongGame model for paddles and ball dynamics and control
@@ -15,8 +34,13 @@ class PongGame(object):
 
     """
     def __init__(s, windowW, windowH):
-        s.windowSize = (windowH, windowW)
+        s.w = windowW
+        s.h = windowH
+        s.size = (s.w, w.h)
+        s.startVelMag = STARTING_VELOCITY_MAGNITUDE
+        s.startVelAngle = STARTING_VELOCITY_ANGLE
 
+    #==== Public Methods ========================================
 
     def start(s):
         """
@@ -29,11 +53,21 @@ class PongGame(object):
         ----------
 
         """
-        pass
+        s.ball.setLoc(s.w/2, s.h/2)
+        s.ball.setVel(s._rndBallVel(s.startVelAngle, s.startVelMag))
+        s.p1.paddle.setLoc(s.w/2)
+        s.p2.paddle.setLoc(s.w/2)
+        s.p1.setScore(0)
+        s.p2.setScore(0)
 
     def step(s):
         """
         Step the game
+        1) Set the paddle locations
+        2) Set ball location
+        3) Check for collisions and update ball location
+        4) Check and update score
+        5) Check win condition
 
         Parameters
         ----------
@@ -41,8 +75,50 @@ class PongGame(object):
         """
         pass
 
+    #==== Private Methods ======================================
+
+    def _rndBallVel(s, angle, mag):
+        """
+        Generate a random velocity with the specified magnitude that points
+        either up or down but no more than the specified angle off of the 
+        horizontal line.
+
+        Parameters
+        ----------
+        s       PongGame object
+        angle   Maximum offset from horizontal in degrees
+        mag     Magnitude of the velocity vector
+
+        Returns
+        -------
+        Velocity tuple (x,y)        
+        """
+        maxX = math.sin(math.radians(angle))
+        x = rnd.uniform(-maxX, maxX)
+        y = 1 - x*x
+
+        # Randomly determine up or down
+        y = y * rnd.sample([-1,1], 1)[0]
+        
+        return (x*mag, y*mag)
+
+#=====================================================
+# Ball
+#=====================================================
+
 class Ball(object):
     pass
+
+#=====================================================
+# Player
+#=====================================================
+
+class Player(object):
+    pass
+
+#=====================================================
+# Paddle
+#=====================================================
 
 class Paddle(object):
     pass
