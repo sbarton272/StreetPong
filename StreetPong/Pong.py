83584d2be6c266e5c58a76e87c60ee00ba84ef95
Spencer Barton <sbarton272@gmail.com>
2015-01-23 13:30:31 -0500

    Rotated?

--- StreetPong/Pong.py
+++ StreetPong/Pong.py
@@ -51,7 +51,7 @@ class PongMaster(object):
         pg.init()
         #s.screen = pg.display.set_mode(s.size, pg.FULLSCREEN)
         s.screen = pg.display.set_mode(s.size)
-        s.screen = pg.transform.rotate(s.screen, 90)
+        s.screen = pg.transform.rotate(s.screen, -90)
 
         s.model = Model.PongGameModel('Player1', 'Player2', s.WIDTH, s.HEIGHT, s.END_ZONE, s.PADDLE_W,
                 s.BALL_RADIUS)
